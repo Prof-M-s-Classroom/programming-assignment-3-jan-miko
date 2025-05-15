36ab4bbec1fa2bad6cdd98b3d0a1daa0b104d2e4
jan-miko <bautistajan425@gmail.com>
2025-05-14 20:16:51 -0700

    added include statement climits and implemented MinHeap constructor and destructor methods

--- heap.h
+++ heap.h
@@ -1,10 +1,34 @@
 #ifndef HEAP_H
 #define HEAP_H
 
+#include <climits> //want to use INT_MAX to represent "infinity"
+
 class MinHeap {
 public:
-    MinHeap(int capacity);
-    ~MinHeap();
+    MinHeap(int capacity) {
+        // initialize and assign arrays and attributes
+        heapArray = new int[capacity];
+        keyArray = new int[capacity];
+        position = new int[capacity];
+        size = 0;
+        this->capacity = capacity;
+
+        heapArray[0] = keyArray[0] = position[0] = 0;
+        size++;
+
+        for (int i = 1; i < capacity; i++) {
+            heapArray[i] = position[i] = i;
+            keyArray[i] = INT_MAX;
+            size++;
+        }
+    }
+
+    ~MinHeap() {
+        delete heapArray;
+        delete keyArray;
+        delete position;
+    }
+
     void insert(int vertex, int key);
     int extractMin();
     void decreaseKey(int vertex, int newKey);
@@ -15,7 +39,7 @@ private:
     int* heapArray;        // Heap of vertex indices
     int* keyArray;         // Corresponding key values
     int* position;         // Maps vertex to its position in heap
-    int capacity;
+    int capacity;          // maximum amount of nodes allowed in heap
     int size;
 
     void minHeapify(int idx);
