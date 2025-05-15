629e490d4ae92ec6d353842d0c6c6e6c4cf3ab2b
jan-miko <bautistajan425@gmail.com>
2025-05-14 19:41:09 -0700

    added include statements for output and heap class, implemented Graph constructor, destructor, and add edge methods

--- graph.h
+++ graph.h
@@ -1,11 +1,36 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include <iostream>
+using namespace std;
+
+#include "heap.h"
+
 class Graph {
 public:
-    Graph(int vertices);
-    ~Graph();
-    void addEdge(int u, int v, int weight);
+    Graph(int vertices) {
+        numVertices = vertices;
+        adjMatrix = new int*[numVertices];
+        for (int i = 0; i < numVertices; i++)
+            {
+            adjMatrix[i] = new int[numVertices];
+            for (int j = 0; j < numVertices; j++) {
+                if (i == j)
+                    adjMatrix[i][j] = 0;
+                else
+                    adjMatrix[i][j] = INT_MAX;
+            }
+        }
+    }
+
+    ~Graph() {
+        delete adjMatrix;
+    }
+
+    void addEdge(int u, int v, int weight){
+    adjMatrix[u][v] = adjMatrix[v][u] = weight;
+}
+
     void primMST();  // Must print MST edges and total weight
 
 private:
